1e49a7540b9c23d5656cd8598e5837ddb001c909
Neil Bock <ncbock@gmail.com>
2020-04-19 12:00:32 -0400

    Display word, and update counter for wrong guesses

--- hangman.py
+++ hangman.py
@@ -1,12 +1,13 @@
 import pygame
 from string import ascii_uppercase
+from getpass import getpass
 
 #RGB colors for drawing
 white = (255, 255, 255)
 black = (0,0,0)
 green = (0,255, 0)
 
-
+clock = pygame.time.Clock()
 
 def main():
     #word = getWord()
@@ -16,13 +17,13 @@ def main():
 
     pygame.display.set_caption("Hang Man")
 
-    clock = pygame.time.Clock()
-
     #Track the count of wrong guesses the player has made and to start the game.
     count = 0
 
     lettersGuessed = []
     
+    word = ""
+
     while True:
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
@@ -36,12 +37,17 @@ def main():
                     elif pos[1]>=200 and pos[1]<= 240:
                         print("2 Player mode Selected")
                         count += 1
+                        word = getWord()
             if e.type == pygame.KEYDOWN:
                 if chr(e.key).upper() not in lettersGuessed and count >0:
                     lettersGuessed.append(chr(e.key).upper())
+                    if chr(e.key).upper() not in word.upper():
+                        count += 1
         
-        #print(lettersGuessed)        
+        # All drawing to occur after win.fill or else it will be erased.
         win.fill(white)
+        if count >0:
+            displayWord(win, word, lettersGuessed)
         header = pygame.font.SysFont("arial", 65)
         title = header.render("Hangman",1,black)
         win.blit(title,(240,10))
@@ -49,8 +55,6 @@ def main():
         drawHang(win, count)
         drawMan(win, count)
         guessedLetters(win, lettersGuessed)
-        # get_spaces = drawWord(win,len(word))
-        # displayWord(win, get_spaces, word)
         numPlayerSelect(win, count)
         pygame.display.update()
         clock.tick(60)
@@ -82,14 +86,15 @@ def drawHang(win, count):
     pygame.draw.line(win,color,(200,100),(200,350),12)
     # Draw Horizontal
     pygame.draw.line(win,color,(195,100),(350,100),12)
-    # Draw Rope
-    pygame.draw.line(win,color, (340,100),(340,150), 2)
-
+  
 def drawMan(win, count):
     if count == 0:
         color = green
     else:
         color = black
+
+     # Draw Rope
+    pygame.draw.line(win,color, (340,100),(340,150), 2)
     # Draw the head
     pygame.draw.circle(win,color,(340,170),20)
     # Draw the body
@@ -103,33 +108,24 @@ def drawMan(win, count):
     # Draw Right Arm
     pygame.draw.line(win,color, (340,215),(375,250), 2)
 
-# def drawWord(screen, lenWord):
-#     start_x =15
-#     end_x = 35
-#     y = 490
-#     locations = []
-#     for i in range(lenWord):
-#         x_avg = (start_x + end_x)/2
-#         locations.append([x_avg,y-10])
-#         pygame.draw.line(screen,(0,0,0),(start_x,y),(end_x,y))
-#         start_x = end_x + 5
-#         end_x = start_x + 20
-#     return locations
+def displayWord(screen, word, guesses):
+    font = pygame.font.SysFont("Arial", 20)
+    disp = []
+    for abc in word:
+        if abc.upper() in guesses:
+            disp.append(" "+abc.upper()+" ")
+        else:
+            disp.append("__ ")
+    lenWord = len(disp)
+    widthRect = (27.5 * lenWord) + 15
+    rectStart = 375- (widthRect/2)
+    pygame.draw.rect(screen, black, (rectStart,400,widthRect,40),2)
+    display = font.render("".join(disp),1, black)
+    screen.blit(display,(rectStart + 15,412))
 
-def displayWord(screen, locations, word):
-    font = pygame.font.Font(None, 20)
-    for letters in word:
-        text = font.render(letters, True, (0,0,0), (255,255,255))
-        textRect = text.get_rect()
-        for spaces in locations:
-            textRect.center = (spaces[0], spaces[1])
-        screen.blit(text, textRect)
-
-
-# def getWord():
-#     word = input("What word are we making?\n\n")
-#     print(chr(27)+"[2J")
-#     return word
+def getWord():
+    word = getpass("Enter your Word: ")
+    return word
 
 def guessedLetters(win, p):
     guessFont= pygame.font.SysFont("arial",25)
@@ -142,21 +138,6 @@ def guessedLetters(win, p):
         if abc in p:
             pygame.draw.line(win,black,(x_start,487),(x_start+20,487),3)
         x_start += 28
-        
-
-def inputP2(win):
-    input_box = pygame.rect(550,200,125,40)
-
-
-    
-
-
-
-
-
-
-
-
 
 if __name__ == '__main__':
     main()
